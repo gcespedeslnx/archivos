f04166fae8dc5d3dd831bacc3fe2ddba408c0aed
Gerry Cespedes <gcespedeslnx@hotmail.com>
2024-07-23 00:32:52 -0400

    Se suben los arhivos

---
+++ archivo.js
@@ -0,0 +1,12 @@
+
+const fs = require("fs")
+//fs.writeFileSync("nuevoarchivo.txt","este  es un mensaje",{encoding:"utf8" })
+
+//const data = fs.readFileSync("nuevoarchivo.txt","utf8")
+//console.log(data)
+
+//const appe = fs.appendFileSync("nuevoarchivo.txt", "\nSe agega una nueva lines", "utf8")
+//console.log(appe)
+
+//const appe = fs.appendFileSync("nuevoarchivo.txt", "\nSe agega una nueva lines", "utf8")
+//console.log(appe)
